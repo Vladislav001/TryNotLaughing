099f5bc986b7833eee35e7a997785f76eeb4f0b8
Vladislav001 <VladGurjev@mail.ru>
2018-08-20 21:01:35 +0300

    Функция подсчета времени видосов, пока не наберется нужное время

--- routes/api/v1/getlinksforlong.js
+++ routes/api/v1/getlinksforlong.js
@@ -1,24 +1,69 @@
 const Video = require('../../../models/video');
 
 exports.post = function(req, res) {
-  var countVideos = parseInt(req.body.rounds);
-  var minMinutes = parseInt(req.body.min_minutes);
+  var totalTime = parseInt(req.body.total_time);
+  var timeVideos = 0;
   var longVideos = [];
 
-  // Получим рандомные Long видео
+  var timeVideos = Number(0);
+
+//   getTime(Video, function(timeV) {
+//   console.log(timeV + ' callback') ;
+//   timeVideos += timeV;
+//   console.log(timeVideos + ' callback') ;
+// });
+
+getTime(0, totalTime, function(timeV) {
+    console.log(timeV + " finished") ;
+  }
+);
+
+
+
+
+// Получим рандомные Long видео
+// Video.aggregate(
+//   [ { $match: { category: 'Long' } }, { $sample: { size: 1 } }] , function (err, randomLongVideos) {
+//     if (err) return res.status(500).send('Error on the server: ' + err);
+//
+//     // Наберем массив роликов, с общей продолжительностью равной заданной
+//     // Сформируем массив для передачи
+//     // randomLongVideos.forEach(function(element) {
+//     //   longVideos.push(element.link);
+//     // });
+//
+//
+//
+//     res.status(200).send(
+//       {
+//         longVideos
+//       }
+//     );
+//
+//   });
+}
+function getTime(currentTimeVideo, totalTimeVideo, callback) {
   Video.aggregate(
-    [ { $match: { category: 'Long', time: { $gte: minMinutes } } }, { $sample: { size: countVideos } }] , function (err, randomLongVideos) {
+    [ { $match: { category: 'Long' } }, { $sample: { size: 1 } }] ,
+    function (err, randomLongVideos) {
+
       if (err) return res.status(500).send('Error on the server: ' + err);
 
-      // Сформируем массив для передачи
-      randomLongVideos.forEach(function(element) {
-        longVideos.push(element.link);
-      });
+      timeVideo = randomLongVideos[0].time;
+      currentTimeVideo += timeVideo
 
-      res.status(200).send(
-        {
-          longVideos
-        }
-      );
+      console.log( randomLongVideos[0].link + ' link') ;
+      console.log( randomLongVideos[0].time + ' time') ;
+
+      if (currentTimeVideo >= totalTimeVideo) {
+
+          console.log(currentTimeVideo + ' currentTimeVideo') ;
+          console.log(totalTimeVideo + ' totalTimeVideo') ;
+          callback(currentTimeVideo);
+
+      } else {
+        getTime(currentTimeVideo, totalTimeVideo, callback);
+      }
+
     });
   }
