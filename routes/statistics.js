f7f09334d71e777733b37ac72a25c45652be1cd4
Vladislav001 <VladGurjev@mail.ru>
2018-08-21 21:53:37 +0300

    Наброски для отрисовки статы

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,5 +1,14 @@
-//const Statistics = require('../models/statistics');
+const Statistic = require('../models/statistic');
 
 exports.get = function(req, res) {
-    res.render('statistics');
+
+  Statistic.find({},  function(err, statistics) {
+    // console.log(statistics[0].type_game);
+    // console.log(statistics[0].videos[1].link);
+
+    res.render('statistics', {
+      statistics: statistics
+    });
+  });
+
 };
