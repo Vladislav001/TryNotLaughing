df1a1781887b6d9243dc1171f02a2d664f8036e5
Vladislav001 <VladGurjev@mail.ru>
2018-08-22 17:15:13 +0300

    Вывод статистики - примитивно

--- routes/statistics.js
+++ routes/statistics.js
@@ -2,13 +2,20 @@ const Statistic = require('../models/statistic');
 
 exports.get = function(req, res) {
 
+  var statisticsVideos = []
+
   Statistic.find({},  function(err, statistics) {
     // console.log(statistics[0].type_game);
     // console.log(statistics[0].videos[1].link);
 
+
+
+console.log(statisticsVideos);
     res.render('statistics', {
       statistics: statistics
     });
   });
 
 };
+
+Statistic.distinct('type_game')
