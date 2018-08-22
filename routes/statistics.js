4eb8e2d8450d1a1c67cd3929c9630d3ebfb1f5e3
Vladislav001 <VladGurjev@mail.ru>
2018-08-22 19:58:30 +0300

    Поправил апи для сбора статистики

--- routes/statistics.js
+++ routes/statistics.js
@@ -9,20 +9,21 @@ exports.get = function(req, res) {
     // console.log(statistics[0].videos[1].link);
 
     // Соберем массивы со статистикой
-    // for (var i = 0; i < statistics.length; i++) {
-    //   statisticsVideos[statistics[i].type_game] = []; // категория
-    //
-    //   for (var j = 0; j < statistics[i].videos.length; j++) {
-    //     console.log(statistics[i].videos[j].link + ' statistics[i].videos[j].link');
-    //
-    //     statisticsVideos[statistics[i].type_game][statistics[i].videos[j].link] = []; // категория => ссылка
-    //
-    //     statisticsVideos[statistics[i].type_game][statistics[i].videos[j].link]["COUNT_PLAYER_1"] = statistics[i].videos[j].countPlayer1; // категория => ссылка => COUNT_PLAYER_1
-    //     statisticsVideos[statistics[i].type_game][statistics[i].videos[j].link]["COUNT_PLAYER_2"] = statistics[i].videos[j].countPlayer2;
-    //     statisticsVideos[statistics[i].type_game][statistics[i].videos[j].link]["ALL"] = statistics[i].videos[j].countPlayer1 + statistics[i].videos[j].countPlayer2;
-    //   }
-    // }
-    //
+    for (var i = 0; i < statistics.length; i++) {
+      statisticsVideos[statistics[i].id] = []; // категория
+
+      for (var j = 0; j < statistics[i].videos.length; j++) {
+      //  console.log(statistics[i].id[j].link + ' statistics[i].videos[j].link');
+
+        statisticsVideos[statistics[i].id][statistics[i].videos[j].link] = []; // категория => ссылка
+
+        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["TYPE_GAME"] = statistics[i].type_game;
+        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["COUNT_PLAYER_1"] = statistics[i].videos[j].countPlayer1; // категория => ссылка => COUNT_PLAYER_1
+        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["COUNT_PLAYER_2"] = statistics[i].videos[j].countPlayer2;
+        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["ALL"] = statistics[i].videos[j].countPlayer1 + statistics[i].videos[j].countPlayer2;
+      }
+    }
+
     // console.log(statisticsVideos);
 
     // Отфильтруем массивы, если одинаковы ссылки ролико в одной и той же категории - сложим записи
