a6f5bea3d5c908e9afb868cc853f1f4e0359e12a
Vladislav001 <VladGurjev@mail.ru>
2018-08-25 14:29:24 +0300

    Сформировал массив для статы(не фильтрованный)

--- routes/statistics.js
+++ routes/statistics.js
@@ -3,6 +3,8 @@ const Statistic = require('../models/statistic');
 exports.get = function(req, res) {
 
   var statisticsVideos = [];
+  var longVideos = [];
+  var vineVideos = [];
 
   Statistic.find({},  function(err, statistics) {
     // console.log(statistics[0].type_game);
@@ -10,22 +12,31 @@ exports.get = function(req, res) {
 
     // Соберем массивы со статистикой
     for (var i = 0; i < statistics.length; i++) {
-      statisticsVideos[statistics[i].id] = []; // категория
+      var videos = [];
+      videos[statistics[i].type_game] = []; // категория
 
       for (var j = 0; j < statistics[i].videos.length; j++) {
-      //  console.log(statistics[i].id[j].link + ' statistics[i].videos[j].link');
-        statisticsVideos[statistics[i].id][statistics[i].videos[j].link] = []; // категория => ссылка
-
-        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["TYPE_GAME"] = statistics[i].type_game;
-        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["COUNT_PLAYER_1"] = statistics[i].videos[j].countPlayer1; // категория => ссылка => COUNT_PLAYER_1
-        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["COUNT_PLAYER_2"] = statistics[i].videos[j].countPlayer2;
-        statisticsVideos[statistics[i].id][statistics[i].videos[j].link]["ALL"] = statistics[i].videos[j].countPlayer1 + statistics[i].videos[j].countPlayer2;
+        videos[statistics[i].type_game][statistics[i].videos[j].link] = []; // категория => ссылка
+        videos[statistics[i].type_game][statistics[i].videos[j].link]["TYPE_GAME"] = statistics[i].type_game;
+        videos[statistics[i].type_game][statistics[i].videos[j].link]["COUNT_PLAYER_1"] = statistics[i].videos[j].countPlayer1; // категория => ссылка => COUNT_PLAYER_1
+        videos[statistics[i].type_game][statistics[i].videos[j].link]["COUNT_PLAYER_2"] = statistics[i].videos[j].countPlayer2;
+        videos[statistics[i].type_game][statistics[i].videos[j].link]["ALL"] = statistics[i].videos[j].countPlayer1 + statistics[i].videos[j].countPlayer2;
+        statisticsVideos.push(videos);
       }
     }
 
-    // console.log(statisticsVideos);
+    //console.log(statisticsVideos);
 
     // Отфильтруем массивы, если одинаковы ссылки ролико в одной и той же категории - сложим записи
+    statisticsVideos.forEach(function(video) {
+    //  console.log(video);
+
+    Object.keys(video).forEach(function(key) {
+      //console.log(key, ':', this[key]);
+
+    }, video);
+
+    });
 
     res.render('statistics', {
       statistics: statistics
